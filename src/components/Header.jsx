906f1c6446412f2adacd17ffcdbdaca46781def4
Rohitprakash249 <rohitprakash249@gmail.com>
2025-05-20 08:07:42 +0530

    ui changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,14 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="flex justify-evenly items-center h-17 mb-3 p-4 dark:border-gray-700 bg-blue-200">
-      <NavLink to="/"><h1 className="text-xl font-bold">NOTES APP</h1></NavLink>
-      <div className="flex flex-row gap-10 ">
-        <NavLink to={"/"} className="text-[18px]">Home</NavLink>
-        <NavLink to={"/notes"} className="text-[18px]">Notes</NavLink>
+    <header className="flex w-full justify-center items-center h-17 mb-3  dark:border-gray-700 bg-white">
+      <div className="flex px-4 md:px-4 items-center justify-between w-[800px]">
+      <NavLink to="/"><h1 className="text-xl text-black font-bold">NOTES APP</h1></NavLink>
+      <div className="flex flex-row gap-5 ">
+        <NavLink to={"/"} className="btn btn-accent text-[18px] hover:scale-105 ">Home</NavLink>
+        {/* <button className="btn btn-accent">hello</button> */}
+        <NavLink to={"/notes"} className="btn btn-accent text-[18px]  hover:scale-105">Notes</NavLink>
+      </div>
       </div>
     </header>
   );
